634257c70737ff028abaae22bf21a2ec63f38dbb
Paraskevas Paraskevas <paraskevasp@gmail.com>
2025-05-14 22:28:53 +0100

    Implement dynamic programming approach for Fibonacci sequence

--- challenges/fibonacci/src/lib.rs
+++ challenges/fibonacci/src/lib.rs
@@ -1,7 +1,27 @@
-pub fn fibonacci(n: u32) -> u32 {
-    match n {
-        0 => 0,
-        1 => 1,
-        _ => fibonacci(n - 1) + fibonacci(n - 2),
+/// Dynamic Programming implementation of the Fibonacci sequence.
+///
+/// # Arguments
+/// 
+/// * `n`:  u32, the nth number in the fibonacci sequence
+///
+/// returns: u32  the result of the nth number in the fibonacci sequence
+///
+/// # Examples
+///
+/// ```
+/// use fibonacci::fibonacci_dp;
+/// let result = fibonacci_dp(4);
+/// assert_eq!(result, 3);
+//
+/// ```
+pub fn fibonacci_dp(n: u32) -> u32 {
+    if n == 0 {
+        return 0;
     }
+    let mut fib = vec![0; (n + 1) as usize];
+    fib[1] = 1;
+    for i in 2..=n as usize {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    fib[n as usize]
 }
